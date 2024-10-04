beff4677f3fedd8013504bbf4adbe31d91063529
rahedhossen49 <rahedhossen49@gmail.com>
2024-10-04 22:00:35 +0600

    first commit

--- resources/views/Frontend/Category_Archive.blade.php
+++ resources/views/Frontend/Category_Archive.blade.php
@@ -1,135 +1,107 @@
-
-
 @extends('layouts.FrontendLayout')
-
 
 @section('content')
 
+<div class="axil-breadcrumb-area">
+    <div class="container">
+        <div class="row align-items-center">
+            <div class="col-lg-6 col-md-8">
+                <div class="inner">
+                    <h1 class="title">{{str(request()->slug ?? 'Others')->headline}} Categories</h1>
+                </div>
+            </div>
+            <div class="col-lg-6 col-md-4">
+                <div class="inner">
+                    <div class="bradcrumb-thumb">
+                        <img src="{{asset('Frontend/assets/images/product/product-45.png')}}" alt="Image">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 
+<div class="axil-shop-area axil-section-gap bg-color-white">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="axil-shop-top">
+                    <div class="row justify-content-end">
+                        <div class="col-lg-3">
+                            <div class="category-select mt_md--10 mt_sm--10 justify-content-lg-end">
+                                <select class="single-select">
+                                    <option>Sort by Latest</option>
+                                    <option>Sort by Name</option>
+                                    <option>Sort by Price</option>
+                                    <option>Sort by Viewed</option>
+                                </select>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
 
-
-
-        <!-- Start Breadcrumb Area  -->
-        <div class="axil-breadcrumb-area">
-            <div class="container">
-                <div class="row align-items-center">
-                    <div class="col-lg-6 col-md-8">
+        <div class="row row--15">
+            @foreach ($products as $product)
+            <div class="col-xl-3 col-lg-4 col-sm-6">
+                <div class="axil-product product-style-one has-color-pick mt--40">
+                    <div class="thumbnail">
+                        <a href="single-product.html">
+                            <img data-sal="zoom-out" data-sal-delay="200" data-sal-duration="800" loading="lazy" class="main-img sal-animate"
+                            src="{{$product->image ? asset('storage/' . $product->image) : asset(env('PLACEHOLDER')) }}" alt="{{$product->slug}}">
+                            <img class="hover-img" src="{{ $product->featuredGallery->isNotEmpty() ? asset('storage/'. $product->featuredGallery->first()->image) : asset(env('PLACEHOLDER')) }}" alt="{{$product->slug}}">
+                        </a>
+                        <div class="label-block label-right">
+                            <div class="product-badget">20% OFF</div>
+                        </div>
+                        <div class="product-hover-action">
+                            <ul class="cart-action">
+                                <li class="wishlist"><a href="wishlist.html"><i class="far fa-heart"></i></a></li>
+                                <li class="select-option"><a href="cart.html">Add to Cart</a></li>
+                                <li class="quickview"><a href="#" data-bs-toggle="modal" data-bs-target="#quick-view-modal"><i class="far fa-eye"></i></a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="product-content">
                         <div class="inner">
-
-                            <h1 class="title">{{str(request()->slug ?? 'Others')->headline}} Categories</h1>
-                        </div>
-                    </div>
-                    <div class="col-lg-6 col-md-4">
-                        <div class="inner">
-                            <div class="bradcrumb-thumb">
-                                <img src="assets/images/product/product-45.png" alt="Image">
+                            <h5 class="title"><a href="single-product.html">{{$product->title}}</a></h5>
+                            <div class="product-price-variant">
+                                @if ($product->selling_price)
+                                <span class="price current-price">{{$product->selling_price}}Taka</span>
+                                <span class="price old-price">{{$product->price}}Taka</span>
+                                @else
+                                <span class="price old-price">{{$product->price}}Taka</span>
+                                @endif
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
+            @endforeach
         </div>
-        <!-- End Breadcrumb Area  -->
-        <!-- Start Shop Area  -->
-        <div class="axil-shop-area axil-section-gap bg-color-white">
-            <div class="container">
-                <div class="row">
-                    <div class="col-lg-12">
-                        <div class="axil-shop-top">
-                            <div class="row justify-content-end">
+    </div>
 
-                                <div class="col-lg-3">
-                                    <div class="category-select mt_md--10 mt_sm--10 justify-content-lg-end">
-                                        <!-- Start Single Select  -->
-                                        <select class="single-select">
-                                            <option>Sort by Latest</option>
-                                            <option>Sort by Name</option>
-                                            <option>Sort by Price</option>
-                                            <option>Sort by Viewed</option>
-                                        </select>
-                                        <!-- End Single Select  -->
-                                    </div>
-                                </div>
+    <div class="text-center pt--30">
+        <a href="#" class="axil-btn btn-bg-lighter btn-load-more">Load more</a>
+    </div>
+</div>
 
+<div class="axil-newsletter-area axil-section-gap pt--0">
+    <div class="container">
+        <div class="etrade-newsletter-wrapper bg_image bg_image--5">
+            <div class="newsletter-content">
+                <span class="title-highlighter highlighter-primary2"><i class="fas fa-envelope-open"></i>Newsletter</span>
+                <h2 class="title mb--40 mb_sm--30">Get weekly update</h2>
+                <div class="input-group newsletter-form">
+                    <div class="position-relative newsletter-inner mb--15">
+                        <input placeholder="example@gmail.com" type="text">
                     </div>
-                </div>
-                <div class="row row--15 ">
-                    @foreach ($products as $product)
-                    <div class="col-xl-3 col-lg-4 col-sm-6">
-                        <div class="axil-product product-style-one has-color-pick mt--40">
-                            <div class="thumbnail">
-                                <a href="single-product.html">
-
-
-                                    <img data-sal="zoom-out" data-sal-delay="200" data-sal-duration="800" loading="lazy" class="main-img sal-animate"
-                                    src="{{$product->image ? asset('storage/' . $product->image) : asset(env('PLACEHOLDER')) }}" alt="{{$product->slug}}">
-
-                                    <img class="hover-img" src="{{ $product->featuredGallery->isNotEmpty() ?
-                                     asset('storage/'. $product->featuredGallery->first()->image) : asset(env('PLACEHOLDER')) }}" alt="{{$product->slug}}">
-                                </a>
-                                <div class="label-block label-right">
-                                    <div class="product-badget">20% OFF</div>
-                                </div>
-                                <div class="product-hover-action">
-                                    <ul class="cart-action">
-                                        <li class="wishlist"><a href="wishlist.html"><i class="far fa-heart"></i></a></li>
-                                        <li class="select-option"><a href="cart.html">Add to Cart</a></li>
-                                        <li class="quickview"><a href="#" data-bs-toggle="modal" data-bs-target="#quick-view-modal"><i class="far fa-eye"></i></a></li>
-                                    </ul>
-                                </div>
-                            </div>
-                            <div class="product-content">
-                                <div class="inner">
-                                    <h5 class="title"><a href="single-product.html">{{$product->title}}</a></h5>
-                                    <div class="product-price-variant">
-                                        @if ($product->selling_price)
-                                        <span class="price current-price">{{$product->selling_price}}Taka</span>
-                                        <span class="price old-price">{{$product->price}}Taka</span>
-                                        @else{
-                                            <span class="price old-price">{{$product->price}}Taka</span>
-                                        }
-                                        @endif
-
-                                    </div>
-
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    @endforeach
-
-                    <!-- End Single Product  -->
-
-
-                </div>
-                <div class="text-center pt--30">
-                    <a href="#" class="axil-btn btn-bg-lighter btn-load-more">Load more</a>
+                    <button type="submit" class="axil-btn mb--15">Subscribe</button>
                 </div>
             </div>
-            <!-- End .container -->
         </div>
-        <!-- End Shop Area  -->
-        <!-- Start Axil Newsletter Area  -->
-        <div class="axil-newsletter-area axil-section-gap pt--0">
-            <div class="container">
-                <div class="etrade-newsletter-wrapper bg_image bg_image--5">
-                    <div class="newsletter-content">
-                        <span class="title-highlighter highlighter-primary2"><i class="fas fa-envelope-open"></i>Newsletter</span>
-                        <h2 class="title mb--40 mb_sm--30">Get weekly update</h2>
-                        <div class="input-group newsletter-form">
-                            <div class="position-relative newsletter-inner mb--15">
-                                <input placeholder="example@gmail.com" type="text">
-                            </div>
-                            <button type="submit" class="axil-btn mb--15">Subscribe</button>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <!-- End .container -->
-        </div>
-        <!-- End Axil Newsletter Area  -->
-
-
-
+    </div>
+</div>
 
 @endsection
